d5acb49bc190a30756c4ba628ffdbf76f2a294e5
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-08-23 01:20:36 +0530

    Revert "Revert "feat: add initial seed data for units, lessons, challenges, and challenge options""
    This reverts commit f86003fb2fe4f08024c226e5b84b72d040335f94.

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -13,6 +13,11 @@ const main = async () => {
     console.log('Seeding database');
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challengeProgress);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challenges);
 
     await db.insert(schema.courses).values([
       {
@@ -44,6 +49,62 @@ const main = async () => {
         id: 6,
         title: 'Croatian',
         imageSrc: '/hr.svg',
+      },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 1,
+        title: 'Unit 1',
+        description: 'Learn the basics of German',
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        order: 1,
+        title: 'Nouns',
+      },
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        type: 'SELECT',
+        order: 1,
+        question: 'Which one of these is the "Father"',
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1,
+        imageSrc: '/man.svg',
+        correct: true,
+        text: 'Vater',
+        audioSrc: '/ge_man.mp3',
+      },
+      {
+        id: 2,
+        challengeId: 1,
+        imageSrc: '/woman.svg',
+        correct: false,
+        text: 'Mutter',
+        audioSrc: '/ge_woman.mp3',
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        imageSrc: '/brother.svg',
+        correct: false,
+        text: 'Bruder',
+        audioSrc: '/ge_brother.mp3',
       },
     ]);
 
